7bf5c514419234a33e325890a8945abd64d60043
chrisabbod <chrisabbod@gmail.com>
2019-01-06 20:06:22 -0500

    Can use keyboard to type number into calculator

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,12 +12,12 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
-    
+    var isFinishedTypingNumber : Bool = true
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
-    
+        isFinishedTypingNumber = true
     }
 
     
@@ -25,6 +25,15 @@ class ViewController: UIViewController {
         
         //What should happen when a number is entered into the keypad
     
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text = displayLabel.text! + numValue
+            }
+        }
     }
 
 }
