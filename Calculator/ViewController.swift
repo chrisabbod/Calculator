4a794c8f8ea029ae367d4f14493f7e5fb6541724
chrisabbod <chrisabbod@gmail.com>
2019-01-06 21:56:14 -0500

    Add functionality to decimal button

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -47,6 +47,20 @@ class ViewController: UIViewController {
                 displayLabel.text = numValue
                 isFinishedTypingNumber = false
             } else {
+                
+                if numValue == "." {
+                    
+                    guard let currentDisplayValue = Double(displayLabel.text!) else {
+                        fatalError("Cannot convert display label text to a double.")
+                    }
+                    
+                    let isInt = floor(currentDisplayValue) == currentDisplayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                    
+                }
                 displayLabel.text = displayLabel.text! + numValue
             }
         }
